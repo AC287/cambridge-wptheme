f89c66a1c0631cbb269761b47c78768fb3fc1bbf
AC287 <arthurchen287@gmail.com>
2017-08-17 10:40:02 -0400

    successfully added bootstarp

--- functions.php
+++ functions.php
@@ -6,10 +6,16 @@
 // add_action('wp_enqueue_scripts','cambridge_resources');
 
 function cambridge_script_enqueue() {
-
+  wp_enqueue_style('bootstrapcss',get_template_directory_uri().'/css/bootstrap.css',array(),null,'all');
+  // wp_enqueue_style('bootstrapFontsEot',get_template_directory_uri().'/fonts/glyphicons-halflings-regular.eot',array(),null,'all');
+  // wp_enqueue_style('bootstrapFontsSvg',get_template_directory_uri().'/fonts/glyphicons-halflings-regular.svg',array(),null,'all');
+  // wp_enqueue_style('bootstrapFontsTtf',get_template_directory_uri().'/fonts/glyphicons-halflings-regular.ttf',array(),null,'all');
+  // wp_enqueue_style('bootstrapFontsWoff',get_template_directory_uri().'/fonts/glyphicons-halflings-regular.woff',array(),null,'all');
+  // wp_enqueue_style('bootstrapFontsWoff2',get_template_directory_uri().'/fonts/glyphicons-halflings-regular.woff2',array(),null,'all');
   wp_enqueue_style('customstyle',get_template_directory_uri().'/css/cambridge.css', array(), '1.0.0', 'all');
   // wp_enqueue_script('jquery');
-  wp_enqueue_script('customjs',get_template_directory_uri().'/js/cambridge.js', array('jquery'), null, true);
+  wp_enqueue_script('bootstrapjs',get_template_directory_uri().'/js/bootstrap.js',array('jquery'),'1.0.0',true);
+  wp_enqueue_script('customjs',get_template_directory_uri().'/js/cambridge.js', array('jquery'), '1.0.0', true);
 
 }
 
--- js/cambridge.js
+++ js/cambridge.js
@@ -2,6 +2,7 @@
 // console.log('Javascript is running');
 jQuery(document).ready(function($){
   $('.top-nav').click(function(){
-    console.log('CLICKED');
+    console.log('CLICKED HEHEHE');
   })
+  console.log('Today is 8/17');
 })
