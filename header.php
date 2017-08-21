fd06778ce3d4e3704a639f6f08d4358754b88beb
AC287 <arthurchen287@gmail.com>
2017-08-21 12:26:51 -0400

    header and footer success.

--- header.php
+++ header.php
@@ -40,12 +40,12 @@
                   <tr>
                     <td class="navi1-btn">
                       <a class="home" href="<?php echo home_url();?>">HOME</a>
-                      <a class="products" href="products/">PRODUCTS</a>
-                      <a class="about" href="about/">ABOUT</a>
-                      <a class="industries" href="industries/">INDUSTRIES</a>
-                      <a class="team" href="team/">OUR TEAM</a>
-                      <a class="brands" href="brands/">BRANDS</a>
-                      <a class="contact" href="contact/">CONTACT</a>
+                      <a class="products" href="<?php echo home_url();?>/products/">PRODUCTS</a>
+                      <a class="about" href="<?php echo home_url();?>/about/">ABOUT</a>
+                      <a class="industries" href="<?php echo home_url();?>/industries/">INDUSTRIES</a>
+                      <a class="team" href="<?php echo home_url();?>/team/">OUR TEAM</a>
+                      <a class="brands" href="<?php echo home_url();?>/brands/">BRANDS</a>
+                      <a class="contact" href="<?php echo home_url();?>/contact/">CONTACT</a>
                     </td>
                   </tr>
                 </table> <!-- end tn-inner-table  -->
@@ -57,7 +57,38 @@
       </div>  <!-- end top-nav -->
       <div class="top-nav2">
         <div class="container">
-          <span class="nav2">OUR BRANDS</span>
+          <table class="tn2-overall-table">
+            <td>
+              <span class="nav2">OUR BRANDS</span>
+              <span class="nav2-logo">
+                <a href="http://www.codaresources.com/" target="_blank" rel="noopener noreferrer">
+                  <img src="<?php bloginfo('template_directory')?>/images/CODA DEV_LOGO.png" height="40px" width="50px">
+                </a>
+              </span>
+              <span class="nav2-logo">
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <img src="<?php bloginfo('template_directory')?>/images/AMRAM_Logo_001.png" height="20px">
+                </a>
+              </span>
+              <span class="nav2-logo">
+                <a href="http://www.ldrind.com/" target="_blank" rel="noopener noreferrer">
+                  <img src="<?php bloginfo('template_directory')?>/images/LDR_LOGO.png" height="25px">
+                </a>
+              </span>
+              <span class="nav2-logo">
+                <a href="" target="_blank" rel="noopener noreferrer">
+                  <img src="<?php bloginfo('template_directory')?>/images/pipedecor_logo.png" height="12px">
+                </a>
+              </span>
+            </td>
+            <td class="nav2-search">
+              <span class="glyphicon glyphicon-search nav2-search-icon"></span>
+              <span class="nav2-search-txt">PRODUCT SEARCH</span>
+            </td>
+            <td class="nav2-search">
+              <input type="text" class="form-control search-field" placeholder="KEYWORD / PHRASE / PART#"></input>
+            </td>
+          </table>  <!--  end tn2-overall-table  -->
         </div>  <!--  end top-nav2 container  -->
       </div> <!-- end top-nav2 -->
       <div class="container">
