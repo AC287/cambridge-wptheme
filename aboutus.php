b24c997642d52383bcc5a9fe74a986e3ac0fa25d
AC287 <arthurchen287@gmail.com>
2017-08-22 15:12:33 -0400

    aboutus partial done.

--- aboutus.php
+++ aboutus.php
@@ -26,6 +26,110 @@
 	<div class="aboutus-timelineimg">
 		<img src="<?php bloginfo('template_directory')?>/images/timeline.png">
 	</div>
+</div>	<!--  end container  -->
+<div class="about-hvac">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="about-cat-img">
+					<img src="<?php bloginfo('template_directory')?>/images/about/about-hvac.png">
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="about-cat-desc">
+					<div class="about-cat-title">
+						HVAC
+					</div>
+					<div class="about-cat-txt">
+						<p class="about-cat-txt-main">
+							CambridgeTM has been a leading manufacturer of premium HVAC products for almost two decades. Duct and pipe sup- port straps conform to UL standards and have been found to be suitable for use in plenums .
+						</p>
+						<p class="about-cat-txt-tags">
+							Duct Straps, Support Webbing, Gas Connectors, Pipe Hanging, Chip Brushes, and more
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="about-electrical">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="about-cat-desc">
+					<div class="about-cat-title">
+						ELECTRICAL
+					</div>
+					<div class="about-cat-txt">
+						<p class="about-cat-txt-main">
+							CambridgeTM dominates in the electrical industry providing many electrical accessories and electrical product support. In an effort to constantly improve the electrician’s/DIY’er’s experience, Cambridge is always looking to develop and offer better alternatives to the current standards out there.
+						</p>
+						<p class="about-cat-txt-tags">
+							Duct Straps, Support Webbing, Gas Connectors, Pipe Hanging, Chip Brushes, and more
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="about-cat-img">
+					<img src="<?php bloginfo('template_directory')?>/images/about/about-electrical.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="about-plumbing">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="about-cat-img">
+					<img src="<?php bloginfo('template_directory')?>/images/about/about-plumbing.jpg">
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="about-cat-desc">
+					<div class="about-cat-title">
+						PLUMBING
+					</div>
+					<div class="about-cat-txt">
+						<p class="about-cat-txt-main">
+							It may look pretty on the outside but CambridgeTM wants to make sure it’s running efficiently on the inside. Keep your pipes and air lines supported by using Cambridge’s broad offerings, many of which conform to UL standards.
+						</p>
+						<p class="about-cat-txt-tags">
+							Hose clamps, Quick release clamps, Pipe hanging support, Gas connec- tors, Fittings, Nipples, Pipe, Valves.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="about-automotive">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="about-cat-desc">
+					<div class="about-cat-title">
+						AUTOMOTIVE
+					</div>
+					<div class="about-cat-txt">
+						<p class="about-cat-txt-main">
+							CambridgeTM is going at full speed to help organize and secure the hoses and pipes in all types of vehicles. Cars, trucks, boats, ATV’s, and more can benefit from the parts that CambridgeTM offers.
+						</p>
+						<p class="about-cat-txt-tags">
+							Standard Hose Clamps, Fuel Injector clamps, Heavy duty t-bolt clamps, Wire connectors, Terminals, Switches, Wire management, Cable Ties, Electrical Tape, Tools, Support Kits.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6">
+				<div class="about-cat-img">
+					<img src="<?php bloginfo('template_directory')?>/images/about/about-automotive.png">
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php get_footer();
