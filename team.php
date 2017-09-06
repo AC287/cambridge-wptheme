426decf61f22c675db00ad770250cc1ea04f46dd
AC287 <arthurchen287@gmail.com>
2017-09-06 11:22:31 -0400

    convert personnel data to database.

--- team.php
+++ team.php
@@ -10,168 +10,26 @@
       <div class="team-personnels-maintitle-underline">
       </div>
     </div>
-    <div class="row">
-      <div class="col-sm-3">
-        <div class="team-individualimg team-hilleltropper">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/hilleltropper.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            HILLEL TROPPER
-          </div>
-          <div class="team-individualtitle">
-            CEO
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-moshetropper">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/moshetropper.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            MOSHE TROPPER
-          </div>
-          <div class="team-individualtitle">
-            PRESIDENT
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-stevekeilson">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/stevekeilson.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            STEVE KEILSON
-          </div>
-          <div class="team-individualtitle">
-            VP SALES &amp MARKETING
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-baruchtravitsky">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/baruchtravitsky.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            BARUCH TRAVITSKY
-          </div>
-          <div class="team-individualtitle">
-            CFO
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-3">
-        <div class="team-individualimg team-vincentwan">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/vincentwan.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            VINCENT WAN
-          </div>
-          <div class="team-individualtitle">
-            GENERAL MANAGER, CODA CHINA
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-stuartneuman">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/stuartneuman.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            STUART NEUMAN
-          </div>
-          <div class="team-individualtitle">
-            NATIONAL SALES MANAGER, CODA RSS
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-charnieadler">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/charnieadler.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            CHARNIE ADLER
-          </div>
-          <div class="team-individualtitle">
-            DIRECTOR OF LOGISTICS
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-cathyparrish">
-          <div class="team-crop-cathy">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/cathyparrish.png" style="height:175px">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            CATHY PARRISH
-          </div>
-          <div class="team-individualtitle">
-            DIRECTOR OF CUSTOMER SERVICE &amp PURCHASING
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-sm-3">
-        <div class="team-individualimg team-davidabraham">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/davidabraham.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            DAVID ABRAHAM
-          </div>
-          <div class="team-individualtitle">
-            WAREHOUSE MANAGER
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-chuckrichards">
-          <div class="team-crop">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/chuckrichards.png">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            CHUCK RICHARDS
-          </div>
-          <div class="team-individualtitle">
-            PRODUCT MANAGER/CATEGORY ANALYST
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-        <div class="team-individualimg team-miriamrosner">
-          <div class="team-crop-miriamrosner">
-            <img src="<?php bloginfo('template_directory')?>/images/personnel/miriamrosner.png" style="height:175px">
-          </div>
-          <hr class="team-breakline"/>
-          <div class="team-individualname">
-            MIRIAM ROSNER
-          </div>
-          <div class="team-individualtitle">
-            DIRECTOR OF MARKETING
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-3">
-      </div>
-    </div>
+
+    <div class="team-wrapper">
+    <?php
+      global $wpdb;
+
+      $team = $wpdb->get_results("SELECT * FROM wp_personnel ORDER BY sort ASC;");
+      // print_r(sizeof($team));
+      foreach($team as $team) {
+        echo "<div class='team-individualimg'>";
+          echo "<div class='team-crop'>";
+            echo "<img src='".$team->img."'>";
+          echo "</div>";
+          echo "<hr class='team-breakline'/>";
+          echo "<div class='team-individualname'>".$team->first." ".$team->last."</div>";
+          echo "<div class='team-individualtitle'>".$team->title."</div>";
+        echo "</div>";
+      }
+
+      ?>
+    </div> <!-- END TEAM WRAPPER -->
   </div>
 
 </div>  <!--  end container -->
