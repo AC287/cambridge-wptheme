4a94a2336d40d3b33d0a5bae75d4aa3dae81b1bf
AC287 <arthurchen287@gmail.com>
2017-09-29 13:00:47 -0400

    testing with sample is success.

--- brands.php
+++ brands.php
@@ -2,78 +2,34 @@
 
 <?php get_header(); ?>
 
-<div class="brands-containeroverall">
+<div class="brands-backcolor">
   <div class="container">
-    <div class="brands-containerinner"> <!-- for flex container? -->
-      <div class="brands-each"> <!-- individual brand container CODA DEV -->
-        <div class="brands-eachmain">
-          <div class="brands-mainimg">
-            <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
-          </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/CODA DEV_LOGO.png">
-          </div>
-        </div>
+    <div class="overall-brands-container">
+      <div class="brands-container">
+        <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
-          <div class="brands-overlay-text">
-            <p></p>
-          </div>
+          <div class="brands-text">Hello World</div>
         </div>
-      </div>  <!--   end brands-each  -->
-      <div class="brands-each"> <!-- individual brand container AMRAM -->
-        <div class="brands-eachmain">
-          <div class="brands-mainimg">
-            <img src="<?php bloginfo('template_directory')?>/images/brands/amram.png">
-          </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/AMRAM_Logo_001.png">
-          </div>
-        </div>
+      </div> <!--  end brands-container  -->
+      <div class="brands-container">
+        <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
-          <div class="brands-overlay-text">
-            <p></p>
-          </div>
+          <div class="brands-text">Hello World</div>
         </div>
-      </div>  <!--   end brands-each  -->
-      <div class="brands-each"> <!-- individual brand container LDR  -->
-        <div class="brands-eachmain">
-          <div class="brands-mainimg">
-            <img src="<?php bloginfo('template_directory')?>/images/brands/ldr.png">
-          </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/LDR_LOGO.png">
-          </div>
-        </div>
+      </div> <!--  end brands-container  -->
+      <div class="brands-container">
+        <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
-          <div class="brands-overlay-text">
-            <p></p>
-          </div>
+          <div class="brands-text">Hello World</div>
         </div>
-      </div>  <!--   end brands-each  -->
-      <div class="brands-each"> <!-- individual brand container PipeDecor -->
-        <div class="brands-eachmain">
-          <div class="brands-mainimg">
-            <img src="<?php bloginfo('template_directory')?>/images/brands/pipedecor.png">
-          </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/pipedecor_logo.png">
-          </div>
-        </div>
+      </div> <!--  end brands-container  -->
+      <div class="brands-container">
+        <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
-          <div class="brands-overlay-text">
-            <p></p>
-          </div>
+          <div class="brands-text">Hello World</div>
         </div>
-      </div>  <!--   end brands-each  -->
-    </div>  <!--  end brands-containerinner  -->
+      </div> <!--  end brands-container  -->
+    </div>  <!--  end overall-brands-container  -->
   </div>
 </div>
 
