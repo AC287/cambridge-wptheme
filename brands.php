8bdf817ddc41a8c7188b9d75227963ccae85e712
AC287 <arthurchen287@gmail.com>
2017-09-29 12:40:32 -0400

    using flex. Fail attempt.

--- brands.php
+++ brands.php
@@ -10,15 +10,16 @@
           <div class="brands-mainimg">
             <img src="<?php bloginfo('template_directory')?>/images/brands/codadev.png">
           </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/CODA DEV_LOGO.png">
+          <div class="brands-logo" style="">
+            <img src="<?php bloginfo('template_directory')?>/images/CODA DEV_LOGO.png" height="80px" width="100px">
           </div>
         </div>
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
+          <!-- <div class="brands-overlay-logo">
+
+          </div> -->
           <div class="brands-overlay-text">
-            <p></p>
+            <p>Some text for CODA DEV</p>
           </div>
         </div>
       </div>  <!--   end brands-each  -->
@@ -27,15 +28,16 @@
           <div class="brands-mainimg">
             <img src="<?php bloginfo('template_directory')?>/images/brands/amram.png">
           </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/AMRAM_Logo_001.png">
+          <div class="brands-logo" style="padding-top: 20px;">
+            <img src="<?php bloginfo('template_directory')?>/images/AMRAM_Logo_001.png" height="40px">
           </div>
         </div>
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
+          <!-- <div class="brands-overlay-logo">
+
+          </div> -->
           <div class="brands-overlay-text">
-            <p></p>
+            <p>Some text for Amram</p>
           </div>
         </div>
       </div>  <!--   end brands-each  -->
@@ -44,15 +46,18 @@
           <div class="brands-mainimg">
             <img src="<?php bloginfo('template_directory')?>/images/brands/ldr.png">
           </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/LDR_LOGO.png">
+          <div class="brands-logo" style="padding-top: 15px;">
+            <img src="<?php bloginfo('template_directory')?>/images/LDR_LOGO.png" height="50px">
           </div>
         </div>
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
+          <!-- <div class="brands-overlay-logo">
+
+          </div> -->
           <div class="brands-overlay-text">
-            <p></p>
+            <p>LDR Global Industries LLC is the leading supplier of high quality kitchen, bath, plumbing and related products to the home improvement industry.</p>
+            <p>Products include hundreds of sku's in Rough Plubing, and carded plumbing.</p>
+            <a href="http://www.ldrind.com" target="_blank" rel="noopener noreferrer">Visit Site >> </a>
           </div>
         </div>
       </div>  <!--   end brands-each  -->
@@ -61,15 +66,18 @@
           <div class="brands-mainimg">
             <img src="<?php bloginfo('template_directory')?>/images/brands/pipedecor.png">
           </div>
-          <div class="brands-logo">
-            <img src="<?php bloginfo('template_directory')?>/images/pipedecor_logo.png">
+          <div class="brands-logo" style="padding-top: 25px;">
+            <img src="<?php bloginfo('template_directory')?>/images/pipedecor_logo.png" height="24px">
           </div>
         </div>
         <div class="brands-overlay">
-          <div class="brands-overlay-logo">
-          </div>
+          <!-- <div class="brands-overlay-logo">
+
+          </div> -->
           <div class="brands-overlay-text">
-            <p></p>
+            <p>From tables, to lamps, shelving units, bed frames, and wine racks, Pipe Decor is the new alternative to mordern furniture and home decoration. With an incredible selection of fittings, nipples, pipe, and exclusively patented specialty items, build whatever your imagination can come up with.</p>
+            <p>Also available kits on the go for easy 1-2-3 assembly.</p>
+            <a href="http://www.pipe-decor.com" target="_blank" rel="noopener noreferrer">Visit Site >> </a>
           </div>
         </div>
       </div>  <!--   end brands-each  -->
--- temp.php
+++ temp.php
@@ -135,3 +135,64 @@
     <input type="text" class="form-control search-field" placeholder="KEYWORD / PHRASE / PART#"></input>
   </td>
 </table>  <!--  end tn2-overall-table  -->
+
+
+<!--  BRANDS  -->
+<div class="brands-each"> <!-- individual brand container AMRAM -->
+  <div class="brands-eachmain">
+    <div class="brands-mainimg">
+      <img src="<?php bloginfo('template_directory')?>/images/brands/amram.png">
+    </div>
+    <div class="brands-logo" style="padding-top: 20px;">
+      <img src="<?php bloginfo('template_directory')?>/images/AMRAM_Logo_001.png" height="40px">
+    </div>
+  </div>
+  <div class="brands-overlay">
+    <!-- <div class="brands-overlay-logo">
+
+    </div> -->
+    <div class="brands-overlay-text">
+      <p>Some text for Amram</p>
+    </div>
+  </div>
+</div>  <!--   end brands-each  -->
+<div class="brands-each"> <!-- individual brand container LDR  -->
+  <div class="brands-eachmain">
+    <div class="brands-mainimg">
+      <img src="<?php bloginfo('template_directory')?>/images/brands/ldr.png">
+    </div>
+    <div class="brands-logo" style="padding-top: 15px;">
+      <img src="<?php bloginfo('template_directory')?>/images/LDR_LOGO.png" height="50px">
+    </div>
+  </div>
+  <div class="brands-overlay">
+    <!-- <div class="brands-overlay-logo">
+
+    </div> -->
+    <div class="brands-overlay-text">
+      <p>LDR Global Industries LLC is the leading supplier of high quality kitchen, bath, plumbing and related products to the home improvement industry.</p>
+      <p>Products include hundreds of sku's in Rough Plubing, and carded plumbing.</p>
+      <a href="http://www.ldrind.com" target="_blank" rel="noopener noreferrer">Visit Site >> </a>
+    </div>
+  </div>
+</div>  <!--   end brands-each  -->
+<div class="brands-each"> <!-- individual brand container PipeDecor -->
+  <div class="brands-eachmain">
+    <div class="brands-mainimg">
+      <img src="<?php bloginfo('template_directory')?>/images/brands/pipedecor.png">
+    </div>
+    <div class="brands-logo" style="padding-top: 25px;">
+      <img src="<?php bloginfo('template_directory')?>/images/pipedecor_logo.png" height="24px">
+    </div>
+  </div>
+  <div class="brands-overlay">
+    <!-- <div class="brands-overlay-logo">
+
+    </div> -->
+    <div class="brands-overlay-text">
+      <p>From tables, to lamps, shelving units, bed frames, and wine racks, Pipe Decor is the new alternative to mordern furniture and home decoration. With an incredible selection of fittings, nipples, pipe, and exclusively patented specialty items, build whatever your imagination can come up with.</p>
+      <p>Also available kits on the go for easy 1-2-3 assembly.</p>
+      <a href="http://www.pipe-decor.com" target="_blank" rel="noopener noreferrer">Visit Site >> </a>
+    </div>
+  </div>
+</div>  <!--   end brands-each  -->
