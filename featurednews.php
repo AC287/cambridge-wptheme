3db206604c4e16b41f17e4a9d9667e5d2b67cc43
AC287 <arthurchen287@gmail.com>
2017-11-15 10:24:45 -0500

    single post success.

---
+++ featurednews.php
@@ -0,0 +1,51 @@
+<?php
+/*
+* Template Name: Featured News
+* Template Post Type: post, news
+*/
+get_header(); ?>
+
+<div class="container news-content-container">
+  <div class="news-mainheader">
+    <span>NEWS</span>
+    <div class="news-mainheader-underline"></div>
+  </div>
+</div>
+<div class="container">
+  <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+          echo "<h1>";
+          the_title();
+          echo "</h1>";
+          echo get_the_date('m-d-Y');
+          the_content();
+
+            /*
+             * Include the post format-specific template for the content. If you want to
+             * use this in a child theme, then include a file called called content-___.php
+             * (where ___ is the post format) and that will be used instead.
+             */
+            get_template_part( 'content', get_post_format() );
+
+            // If comments are open or we have at least one comment, load up the comment template.
+            // if ( comments_open() || get_comments_number() ) :
+            //     comments_template();
+            // endif;
+
+            // Previous/next post navigation.
+            the_post_navigation( array(
+                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'cambridge' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Next post:', 'cambridge' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'cambridge' ) . '</span> ' .
+                    '<span class="screen-reader-text">' . __( 'Previous post:', 'cambridge' ) . '</span> ' .
+                    '<span class="post-title">%title</span>',
+            ) );
+
+        // End the loop.
+        endwhile;
+  ?>
+  <?php printf(wp_get_archives()) ?>
+</div>
+<?php get_footer(); ?>
---
+++ news.php
@@ -0,0 +1,11 @@
+<!--  Template Name: News  -->
+
+<?php get_header(); ?>
+
+<div class="container news-content-container">
+  <div class="news-mainheader">
+    <span>NEWS</span>
+    <div class="news-mainheader-underline"></div>
+  </div>
+</div>
+<?php get_footer(); ?>
