fa91e887083143a8e85d4e1b04b2bb8b616d52a1
AC287 <arthurchen287@gmail.com>
2017-11-16 16:17:05 -0500

    testing with tradeshow sql

--- featurednews.php
+++ featurednews.php
@@ -11,41 +11,64 @@ get_header(); ?>
     <div class="news-mainheader-underline"></div>
   </div>
 </div>
-<div class="container">
-  <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
-          echo "<h1>";
-          the_title();
-          echo "</h1>";
-          echo get_the_date('F d, Y');
-          the_content();
+<div class="container featurednewspage">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="news-upcomingtradeshows-container">
+        <div class="news-uts-title">
+          <span>UPCOMING TRADESHOWS</span>
+        </div>
+        <div class="news-uts-contents">
+          <?php include 'upcomingtradeshows.php';?>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-7">
+      <?php
+      // Start the loop.
+      echo "<div class='featurednews-content'>";
+      while ( have_posts() ) : the_post();
+      echo "<h1>";
+      the_title();
+      echo "</h1>";
+      echo get_the_date('F d, Y');
+      the_content();
+      echo "</div>";
+      /*
+      * Include the post format-specific template for the content. If you want to
+      * use this in a child theme, then include a file called called content-___.php
+      * (where ___ is the post format) and that will be used instead.
+      */
+      get_template_part( 'content', get_post_format() );
 
-            /*
-             * Include the post format-specific template for the content. If you want to
-             * use this in a child theme, then include a file called called content-___.php
-             * (where ___ is the post format) and that will be used instead.
-             */
-            get_template_part( 'content', get_post_format() );
+      // If comments are open or we have at least one comment, load up the comment template.
+      // if ( comments_open() || get_comments_number() ) :
+      //     comments_template();
+      // endif;
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            // if ( comments_open() || get_comments_number() ) :
-            //     comments_template();
-            // endif;
+      // Previous/next post navigation.
+      // the_post_navigation( array(
+      //   'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'cambridgetheme' ) . '</span> ' .
+      //   '<span class="screen-reader-text">' . __( 'post:', 'cambridgetheme' ) . '</span> ' .
+      //   '<span class="post-title">%title</span>',
+      //   'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'cambridgetheme' ) . '</span> ' .
+      //   '<span class="screen-reader-text">' . __( 'post:', 'cambridgetheme' ) . '</span> ' .
+      //   '<span class="post-title">%title</span>',
+      // ) );
 
-            // Previous/next post navigation.
-            the_post_navigation( array(
-                'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'cambridgetheme' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Next post:', 'cambridgetheme' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-                'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'cambridgetheme' ) . '</span> ' .
-                    '<span class="screen-reader-text">' . __( 'Previous post:', 'cambridgetheme' ) . '</span> ' .
-                    '<span class="post-title">%title</span>',
-            ) );
-
-        // End the loop.
-        endwhile;
-  ?>
-  <!-- <?php //printf(wp_get_archives()) ?> -->
+      // End the loop.
+      endwhile;
+      ?>
+      <div class='return-to-news'>
+        <a href="<?php echo home_url();?>/news/">Return to news page.</a>
+      </div>
+      <!-- <?php //printf(wp_get_archives()) ?> -->
+    </div>
+    <div class="col-md-2 news-archive-col">
+      <div class="news-archive">
+        <span>Archive</span>
+      </div>
+    </div>
+  </div>
 </div>
 <?php get_footer(); ?>
---
+++ upcomingtradeshows.php
@@ -0,0 +1,11 @@
+<!--
+**** This will get tradeshow data.
+**** Compare tradeshow start date with current date and display upcoming date.
+-->
+
+<div class="container">
+  <?php
+    global $wpdb;
+    
+  ?>
+</div>
