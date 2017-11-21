b869ea152e17fa09796abd88ddd9de545263e2fd
AC287 <arthurchen287@gmail.com>
2017-11-21 11:39:20 -0500

    added accordion to archive.

--- phpsnippet/archivesnippet.php
+++ phpsnippet/archivesnippet.php
@@ -8,7 +8,6 @@
   if($the_query->have_posts() ) :
     while ( $the_query->have_posts() ) : $the_query->the_post();
       array_push($allYear, get_the_date('Y'));
-      // print_r(get_the_category(slug));
       $tempAllPosts = array(
         'year' => get_the_date('Y'),
         'title' => get_the_title(),
@@ -21,25 +20,14 @@
   $allPost = (object)$allPost;
   $minYear = min($allYear);
   for( $curYear = date('Y'); $curYear >= $minYear; $curYear--) {
-    // echo $curYear;
     echo "<div class='archive-year-container'>";
-      echo "<div class='ayc-year'>";
-        echo "<span class='archive-chev-container'>";
-          echo "<span class='chev-right'>";
-            echo "<img src='".get_bloginfo('template_directory')."/phpsnippet/chev-icon/chev_right.png'/>";
-          echo "</span>";
-        // echo "<span class='chev-down'>";
-        //   include('chev-icon/chev_down.svg');
-        // echo "</span>";
-        echo "</span>";
+      echo "<div class='ayc-year custaccordion'>";
+        echo "<img class='chev' src='http://files.coda.com.s3.amazonaws.com/imgv2/icons/chev-right.png'/>";
         echo "<span>&nbsp;$curYear</span>";
-      echo "</div>";
-      echo "<div class='archive-title'>";
+      echo "</div>";  // end ayc-year
+      echo "<div class='archive-title custpanel'>";
         foreach($allPost as $allPost2) {
-          // echo "for each triggered.";
-          // print_r ($allPost2['title']);
           if($allPost2['year'] == $curYear) {
-            // echo "if triggered.";
             echo "<p><a href=".$allPost2['link'].">".$allPost2['title']."<br/><span class='archive-date'>".$allPost2['postdate']."</span></a></p>";
           }
         }
