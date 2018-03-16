5415abd9ab7257c6a8623e14a35566f21f78c257
AC287 <arthurchen287@gmail.com>
2018-03-16 13:04:03 -0400

    added pagination to tradeshows page. Need styling.

--- news.php
+++ news.php
@@ -20,7 +20,13 @@
     </div>
     <div class="col-md-8 tradeshows-allsection">
       <?php
+      $paged = (get_query_var('paged')) ? get_query_var('paged'):'1';
+      // $page = isset($_GET['cpage']) ? abs((int) $_GET['cpage']) : 1;
+
       $args = array(
+        'nopaging'=>false,
+        'paged'=>$paged,
+        'posts_per_page' =>2,
         'post_type' => 'post',
         // 'ignore_sticky_posts' => 1,
         // 'year'  => date('Y'),
@@ -32,6 +38,11 @@
       $the_query = new WP_Query( $args );
 
       if ( $the_query->have_posts() ) :
+
+        // previous_posts_link('<< Newer Entries');
+        previous_posts_link('<< Newer Entries');
+        next_posts_link('Older Entries>>', $the_query->max_num_pages);
+
         while ( $the_query->have_posts() ) : $the_query->the_post();
         echo "<div class='allpost-link-container'>";
         if(has_post_thumbnail()){
@@ -44,19 +55,35 @@
         }
         echo "<div class='allpost-contents'>";
           echo "<p class='allpost-date'><a href=".get_post_permalink().">".get_the_date('F d, Y')."</a></p>";
-          echo "<span class='tradeshows-post-title'>";
-            the_title();
-          echo "</span>";
-          the_excerpt();  //this will only show excerpt of the tradeshows.
-          echo "<a href=".get_post_permalink().">Click for detail</a>";
-        echo "</div>";
+            echo "<span class='tradeshows-post-title'>";
+              the_title();
+            echo "</span>";
+            the_excerpt();  //this will only show excerpt of the tradeshows.
+            echo "<a href=".get_post_permalink().">Click for detail</a>";
+          echo "</div>";
         echo "</div>";
         echo "<hr/>";
-      endwhile;
-    endif;
+        endwhile;
+        // next_posts_link('Older Entries >>', $the_query->max_num_pages);
 
-    wp_reset_postdata();
-    wp_reset_query();
+        // echo "<div class='ps-pages pagination'>";
+        //   echo paginate_links( array(
+        //     'base' => add_query_arg('cpage','%#%'),
+        //     // 'format' => '',
+        //     'prev_text' => __('&laquo;'),
+        //     'next_text' => __('&raquo;'),
+        //     // 'total' => ceil($total/$items_per_page),
+        //     'current' => $page,
+        //     'mid_size' => 2,
+        //     // 'type' => 'list'
+        //   ));
+        // echo "</div>";
+        previous_posts_link('<< Newer Entries');
+        next_posts_link('Older Entries>>', $the_query->max_num_pages);
+      endif;
+
+      wp_reset_postdata();
+      wp_reset_query();
     ?>
     </div>
     <div class="col-md-2 tradeshows-archive-col">
