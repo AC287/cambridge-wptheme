6b215ca7b49c28c4bda9111f0df92f06914a474c
AC287 <arthurchen287@gmail.com>
2018-06-05 14:44:12 -0400

    removed spec link if the item doesn't have spec sheet.

--- prod-pageitem.php
+++ prod-pageitem.php
@@ -238,9 +238,6 @@
 								if(($get_item_data[0]->spec)!='' || ($get_item_data[0]->spec)){
 									// echo "spec is not empty";
 									echo "<a class='spec-sheet' href='".$get_item_data[0]->spec."' rel='noopener noreferrer' target='_blank'>SPEC SHEET</a>";
-								} else {
-									// echo "spec is empty";
-									echo "<a class='spec-sheet ss-empty'>SPEC SHEET</a>";
 								}
 
 							echo "</div>";	// end item-spec-container div;
